6a43d754a0dde770f47901fbb33a31a0a65727b5
Ajamu Wesley <ajawesley@yahoo.com>
2026-05-02 16:56:28 -0400

    feat: bootstrap ingest service and MVP architecture

---
+++ internal/server/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/ajawes/hesp/internal/ingestion"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func New() *Server {
+	mux := http.NewServeMux()
+
+	handler := ingestion.NewHandler()
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	mux.Handle("/events/ingest", handler)
+
+	return &Server{
+		httpServer: &http.Server{
+			Addr:    ":8080",
+			Handler: mux,
+		},
+	}
+}
+
+func (s *Server) Start() error {
+	log.Printf("starting server on %s", s.httpServer.Addr)
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Stop() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	log.Printf("stopping server")
+	return s.httpServer.Shutdown(ctx)
+}
