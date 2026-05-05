33be2e396c076a41dd1606d2392c624b1184faf5
Ajamu Wesley <ajawesley@yahoo.com>
2026-05-05 04:37:49 -0400

    ensure signal registration occurs prior to server start

--- cmd/ingest-service/main.go
+++ cmd/ingest-service/main.go
@@ -12,17 +12,17 @@ import (
 func main() {
 	srv := server.New()
 
+	// graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
 	go func() {
 		if err := srv.Start(); err != nil {
 			log.Fatalf("server error: %v", err)
 		}
 	}()
 
-	// graceful shutdown
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
 	<-stop
-
 	if err := srv.Stop(); err != nil {
 		log.Printf("graceful shutdown error: %v", err)
 	}
